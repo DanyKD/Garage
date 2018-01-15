0d83926be216a2fa2c5d6eb914d60a9ce59b616c
User <User@DESK-FG2GJ32>
2018-01-15 15:44:28 +0100

    Add project files.

---
+++ Garage/Garage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    class Garage<T> : IEnumerable<T> where T : Vehicle
+    {
+        private T[] _vehicle;
+        private int _count = 0;
+        private int _capacity;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Garage/Vehicle.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    class Vehicle
+    {        
+            private string _registration;
+            private string _color;
+            private int _numberOfWheels;
+            private double _weight;
+            private string _model;
+            public Vehicle(string registration, string color, int numberOfWheels, double weight, string model)
+            {
+                _registration = registration;
+                _color = color;
+                _numberOfWheels = numberOfWheels;
+                _weight = weight;
+                _model = model;           
+            }
+        public string Registration
+        {
+            get
+            {
+                return Registration;
+            }
+            set
+            {
+                Registration = _registration;
+            }
+        }
+        public string Color
+        {
+            get
+            {
+                return Color;
+            }
+            set
+            {
+                Color = _color;
+            }
+        }
+        public int NumberOfWheels
+        {
+            get
+            {
+                return NumberOfWheels;
+            }
+            set
+            {
+                NumberOfWheels = _numberOfWheels;
+            }
+        }
+        public double Weight
+        {
+            get
+            {
+                return Weight;
+            }
+            set
+            {
+                Weight = _weight;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return Model;
+            }
+            set
+            {
+                Model = _model;
+            }
+        }
+    }
+}
